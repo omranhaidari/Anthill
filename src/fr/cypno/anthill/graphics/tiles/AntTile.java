ec19853b88788abe571a650227aa99a2d2cb420f
Pierre Chat <pierrechat89@hotmail.fr>
2017-01-26 15:36:53 +0100

    Ajout des images des fourmis

--- src/fr/cypno/anthill/graphics/tiles/AntTile.java
+++ src/fr/cypno/anthill/graphics/tiles/AntTile.java
@@ -2,9 +2,9 @@ package fr.cypno.anthill.graphics.tiles;
 
 import fr.cypno.anthill.ant.Ant;
 import javafx.scene.Node;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 public class AntTile implements Tile{
     private Ant ant;
@@ -15,12 +15,11 @@ public class AntTile implements Tile{
 
     @Override
     public Node draw(int cellSize) {
-        Text text = new Text(
+        Rectangle rec = new Rectangle(
                 ant.getY() * cellSize + ant.getY(),
-                ant.getX() * cellSize + ant.getX() + cellSize,
-                "1");
-        text.setFont(new Font(cellSize));
-        text.setFill(Color.CYAN);
-        return text;
+                ant.getX() * cellSize + ant.getX(),
+                cellSize, cellSize);
+        rec.setFill(new ImagePattern(new Image("file:ressources/images/ant-" + ant.getDirection() + ".png")));
+        return rec;
     }
 }
